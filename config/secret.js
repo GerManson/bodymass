ab3c9faa4e79c35abef20444fcc1b6adda1caa8e
Germán Rodríguez <germanson@gmail.com>
2017-06-27 18:19:23 -0700

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MAILGUN_API_KEY: 'key-5baa8a5a444214b712a622fb272f5404',
-  MAILGUN_DOMAIN: 'sandboxd159516fc2b840fc97b826a95036b006.mailgun.org',
+  MAILGUN_API_KEY: 'XXX',
+  MAILGUN_DOMAIN: 'XXX',
 };
